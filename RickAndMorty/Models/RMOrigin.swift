7061fefa45d1b1dea0d4260a15aa04a9fe5eac11
Yaroslav <yaraslav.ios.developer@gmail.com>
2023-08-24 21:22:45 +0300

    Add models

---
+++ RickAndMorty/Models/RMOrigin.swift
@@ -0,0 +1,14 @@
+//
+//  RMOrigin.swift
+//  RickAndMorty
+//
+//  Created by Yaroslav on 23.08.23.
+//
+
+import Foundation
+
+
+struct RMOrigin: Codable {
+  let name: String
+  let url: String
+}
---
+++ RickAndMorty/Models/RMSingleLocation.swift
@@ -0,0 +1,14 @@
+//
+//  RMSingleLocation.swift
+//  RickAndMorty
+//
+//  Created by Yaroslav on 23.08.23.
+//
+
+import Foundation
+
+
+struct RMSingleLocation: Codable {
+  let name: String
+  let url: String
+}
