a3c0dae591c6f997793ed0ba50ca8dbc0a1e3ece
Yaroslav <yaraslav.ios.developer@gmail.com>
2023-09-07 22:09:14 +0200

    Refactoring of the structure of the application

---
+++ RickAndMorty/App/ScreenModules/HomeMenu/HomeMenuViewController.swift
@@ -0,0 +1,17 @@
+//
+//  HomeMenuViewController.swift
+//  RickAndMorty
+//
+//  Created by Yaroslav on 7.09.23.
+//
+
+import UIKit
+
+class HomeMenuViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+}
